5412ff58278d44aa27370a0a9a00d0b63ee0a52a
nickleefly <nickleefly@gmail.com>
2014-08-23 15:19:19 -0700

    update gitignore update mysql limit

--- services/graphservice.js
+++ services/graphservice.js
@@ -17,7 +17,7 @@ var GraphService = function() {
     *@return Error to the callback function
     */
     GetDataService: function(callback) {
-      var query = "select m.id, m.release_date, m.movie_title, r.rating, g.genre, u.postalCode from movies m inner join genres_movies gm on gm.movies_id = m.id inner join genres g on gm.genres_id = g.id inner join ratings r on r.movie_id = m.id inner join users u on u.id = r.id limit 10";
+      var query = "select m.id, m.release_date, m.movie_title, r.rating, g.genre, u.postalCode from movies m inner join genres_movies gm on gm.movies_id = m.id inner join genres g on gm.genres_id = g.id inner join ratings r on r.movie_id = m.id inner join users u on u.id = r.id";
 
       mysql.ExecuteQuery(query, function(err, e){
         callback(err, e);
