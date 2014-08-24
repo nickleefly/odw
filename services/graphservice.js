40d25957887f53cdd7a29fa51cdddb4a40fa9235
nickleefly <nickleefly@gmail.com>
2014-08-23 20:58:06 -0700

    limit less data

--- services/graphservice.js
+++ services/graphservice.js
@@ -17,7 +17,7 @@ var GraphService = function() {
     *@return Error to the callback function
     */
     GetDataService: function(callback) {
-      var query = "select sum(r.Rating) as ratings_sum, u.postalCode as postalCode from ratings r inner join users u on u.id = r.id where u.postalCode REGEXP '^-?[0-9]+$' and u.postalCode > 0 group by u.postalCode order by u.postalCode;";
+      var query = "select sum(r.Rating) as ratings_sum, u.postalCode as postalCode from ratings r inner join users u on u.id = r.id where u.postalCode REGEXP '^-?[0-9]+$' and u.postalCode > 0 group by u.postalCode order by u.postalCode limit 200;";
 
       mysql.ExecuteQuery(query, function(err, e){
         callback(err, e);
